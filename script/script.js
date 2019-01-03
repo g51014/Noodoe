d1312f9ccace8c404847fe9e3a56aa56e5d3b9f6
g51014 <a912686931@gmail.com>
2019-01-03 18:05:38 +0800

    Noodoe

---
+++ script/script.js
@@ -0,0 +1,12 @@
+export default class Test extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render()
+  {
+    return(
+      <div>
+        <h1>Test</h1>
+      </div>
+    );
+  }
